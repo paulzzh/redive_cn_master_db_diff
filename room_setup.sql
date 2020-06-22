3cd9c66ab8a36994ef26ce1a1e89f76763c3ec6a
KaguyaMoeBot <104998995+KaguyaMoeBot@users.noreply.github.com>
2020-06-22 14:13:00 +0800

    202006221413

--- room_setup.sql
+++ room_setup.sql
@@ -251,7 +251,7 @@ INSERT INTO `room_setup` VALUES (/*room_item_id*/990107, /*grid_height*/2, /*gri
 INSERT INTO `room_setup` VALUES (/*room_item_id*/990108, /*grid_height*/1, /*grid_width*/2, /*unit_id*/0);
 INSERT INTO `room_setup` VALUES (/*room_item_id*/990109, /*grid_height*/4, /*grid_width*/4, /*unit_id*/0);
 INSERT INTO `room_setup` VALUES (/*room_item_id*/990110, /*grid_height*/4, /*grid_width*/4, /*unit_id*/0);
-INSERT INTO `room_setup` VALUES (/*room_item_id*/990111, /*grid_height*/1, /*grid_width*/3, /*unit_id*/0);
+INSERT INTO `room_setup` VALUES (/*room_item_id*/990111, /*grid_height*/2, /*grid_width*/3, /*unit_id*/0);
 INSERT INTO `room_setup` VALUES (/*room_item_id*/990112, /*grid_height*/1, /*grid_width*/1, /*unit_id*/0);
 INSERT INTO `room_setup` VALUES (/*room_item_id*/990113, /*grid_height*/1, /*grid_width*/1, /*unit_id*/0);
 INSERT INTO `room_setup` VALUES (/*room_item_id*/990115, /*grid_height*/2, /*grid_width*/3, /*unit_id*/0);

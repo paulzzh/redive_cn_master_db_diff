b2c54c8642fc857f644253bdf8f216cb8c288f40
KaguyaMoeBot <104998995+KaguyaMoeBot@users.noreply.github.com>
2021-05-27 14:13:00 +0800

    202105271413

--- chara_fortune_schedule.sql
+++ chara_fortune_schedule.sql
@@ -2,4 +2,3 @@ CREATE TABLE 'chara_fortune_schedule' ('fortune_id' INTEGER NOT NULL, 'name' TEX
 INSERT INTO `chara_fortune_schedule` VALUES (/*fortune_id*/10000, /*name*/'第1届 兰德索尔杯', /*start_time*/'2020/07/11 5:00', /*end_time*/'2020/07/25 4:59');
 INSERT INTO `chara_fortune_schedule` VALUES (/*fortune_id*/18001, /*name*/'第1.5届 兰德索尔杯', /*start_time*/'2021/02/11 5:00', /*end_time*/'2021/02/18 4:59');
 INSERT INTO `chara_fortune_schedule` VALUES (/*fortune_id*/18002, /*name*/'1周年纪念 兰德索尔杯', /*start_time*/'2021/04/18 5:00', /*end_time*/'2021/05/02 4:59');
-INSERT INTO `chara_fortune_schedule` VALUES (/*fortune_id*/18003, /*name*/'端午节 兰德索尔杯', /*start_time*/'2021/06/11 5:00', /*end_time*/'2021/06/21 4:59');

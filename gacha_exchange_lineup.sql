2030799d42ad98ca86ee3c5e3f62da9d1fe344ec
KaguyaMoeBot <104998995+KaguyaMoeBot@users.noreply.github.com>
2021-10-29 10:47:00 +0800

    202110291047

--- gacha_exchange_lineup.sql
+++ gacha_exchange_lineup.sql
@@ -81,3 +81,7 @@ INSERT INTO `gacha_exchange_lineup` VALUES (/*id*/82, /*exchange_id*/52, /*unit_
 INSERT INTO `gacha_exchange_lineup` VALUES (/*id*/83, /*exchange_id*/53, /*unit_id*/108101, /*rarity*/3, /*gacha_bonus_id*/0);
 INSERT INTO `gacha_exchange_lineup` VALUES (/*id*/84, /*exchange_id*/53, /*unit_id*/108301, /*rarity*/3, /*gacha_bonus_id*/0);
 INSERT INTO `gacha_exchange_lineup` VALUES (/*id*/85, /*exchange_id*/54, /*unit_id*/111301, /*rarity*/3, /*gacha_bonus_id*/1002);
+INSERT INTO `gacha_exchange_lineup` VALUES (/*id*/86, /*exchange_id*/55, /*unit_id*/111401, /*rarity*/3, /*gacha_bonus_id*/1003);
+INSERT INTO `gacha_exchange_lineup` VALUES (/*id*/87, /*exchange_id*/56, /*unit_id*/105601, /*rarity*/3, /*gacha_bonus_id*/0);
+INSERT INTO `gacha_exchange_lineup` VALUES (/*id*/88, /*exchange_id*/56, /*unit_id*/101301, /*rarity*/2, /*gacha_bonus_id*/0);
+INSERT INTO `gacha_exchange_lineup` VALUES (/*id*/89, /*exchange_id*/56, /*unit_id*/102701, /*rarity*/2, /*gacha_bonus_id*/0);

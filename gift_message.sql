b63989337fb3db9df8b5f992e5e6497c656c74cc
KaguyaMoeBot <104998995+KaguyaMoeBot@users.noreply.github.com>
2022-01-27 14:43:00 +0800

    202201271443

--- gift_message.sql
+++ gift_message.sql
@@ -82,3 +82,4 @@ INSERT INTO `gift_message` VALUES (/*id*/9702, /*discription*/'在「王都终�
 INSERT INTO `gift_message` VALUES (/*id*/88888, /*discription*/'购买「{0}」获得的道具', /*type_1*/88, /*type_2*/0, /*type_3*/0, /*type_4*/0);
 INSERT INTO `gift_message` VALUES (/*id*/88889, /*discription*/'购买玛那获得的体力', /*type_1*/0, /*type_2*/0, /*type_3*/0, /*type_4*/0);
 INSERT INTO `gift_message` VALUES (/*id*/88890, /*discription*/'购买「女神祭宝库秘钥」获得的道具', /*type_1*/0, /*type_2*/0, /*type_3*/0, /*type_4*/0);
+INSERT INTO `gift_message` VALUES (/*id*/88891, /*discription*/'季票额外经验兑换玛那', /*type_1*/0, /*type_2*/0, /*type_3*/0, /*type_4*/0);

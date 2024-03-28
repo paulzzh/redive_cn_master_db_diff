05e72383ccd56b01b4cda09b1bef5b1796ac38de
KaguyaMoeBot <104998995+KaguyaMoeBot@users.noreply.github.com>
2024-03-28 11:31:00 +0800

    202403281131

--- special_story_banner.sql
+++ special_story_banner.sql
@@ -1,3 +1,4 @@
 CREATE TABLE 'special_story_banner' ('id' INTEGER NOT NULL, 'story_group_id' INTEGER NOT NULL, 'start_time' TEXT NOT NULL, 'remind_end_time' TEXT NOT NULL, 'end_time' TEXT NOT NULL, PRIMARY KEY('id'));
 INSERT INTO `special_story_banner` VALUES (/*id*/80004, /*story_group_id*/9002, /*start_time*/'2023/4/1 5:00:00', /*remind_end_time*/'2023/4/27 23:59:59', /*end_time*/'2023/4/30 23:59:59');
+INSERT INTO `special_story_banner` VALUES (/*id*/80006, /*story_group_id*/9004, /*start_time*/'2024/4/1 5:00:00', /*remind_end_time*/'2024/4/27 23:59:59', /*end_time*/'2024/4/30 23:59:59');
 CREATE INDEX 'special_story_banner_0_story_group_id' on 'special_story_banner'('story_group_id');

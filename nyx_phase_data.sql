fdf1a0510ae3076612c22a7ed21c555d66733f8c
KaguyaMoeBot <104998995+KaguyaMoeBot@users.noreply.github.com>
2024-04-01 15:35:00 +0800

    202404011535

--- nyx_phase_data.sql
+++ nyx_phase_data.sql
@@ -1,4 +1,4 @@
 CREATE TABLE 'nyx_phase_data' ('story_phase' INTEGER NOT NULL, 'phase_title' TEXT NOT NULL, 'condition_quest_id' INTEGER NOT NULL, 'condition_quest_boss' INTEGER NOT NULL, PRIMARY KEY('story_phase'));
-INSERT INTO `nyx_phase_data` VALUES (/*story_phase*/1, /*phase_title*/'特别讲座报告', /*condition_quest_id*/10065105, /*condition_quest_boss*/0);
-INSERT INTO `nyx_phase_data` VALUES (/*story_phase*/2, /*phase_title*/'墨丘利财团活动日记', /*condition_quest_id*/10065111, /*condition_quest_boss*/0);
-INSERT INTO `nyx_phase_data` VALUES (/*story_phase*/3, /*phase_title*/'优妮的记事本', /*condition_quest_id*/10065115, /*condition_quest_boss*/1006501);
+INSERT INTO `nyx_phase_data` VALUES (/*story_phase*/1, /*phase_title*/'特别讲座报告', /*condition_quest_id*/20023105, /*condition_quest_boss*/0);
+INSERT INTO `nyx_phase_data` VALUES (/*story_phase*/2, /*phase_title*/'墨丘利财团活动日记', /*condition_quest_id*/20023111, /*condition_quest_boss*/0);
+INSERT INTO `nyx_phase_data` VALUES (/*story_phase*/3, /*phase_title*/'优妮的记事本', /*condition_quest_id*/20023115, /*condition_quest_boss*/2002301);

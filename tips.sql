d8358bbbe67b77425dda1ddfe13f45908e662db6
KaguyaMoeBot <104998995+KaguyaMoeBot@users.noreply.github.com>
2024-04-03 18:09:00 +0800

    202404031809

--- tips.sql
+++ tips.sql
@@ -115,4 +115,3 @@ INSERT INTO `tips` VALUES (/*id*/4180501, /*value*/180501, /*tips_index*/1, /*ti
 INSERT INTO `tips` VALUES (/*id*/4180601, /*value*/180601, /*tips_index*/1, /*title*/'助威声');
 INSERT INTO `tips` VALUES (/*id*/4180701, /*value*/180701, /*tips_index*/1, /*title*/'我的色彩');
 INSERT INTO `tips` VALUES (/*id*/4180801, /*value*/180801, /*tips_index*/1, /*title*/'总是三个人');
-INSERT INTO `tips` VALUES (/*id*/4180901, /*value*/180901, /*tips_index*/1, /*title*/'两个人一起的话');

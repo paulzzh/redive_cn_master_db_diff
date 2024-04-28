1e36e3ee38d9243ee0532fdd2401da0fa53634bd
KaguyaMoeBot <104998995+KaguyaMoeBot@users.noreply.github.com>
2024-04-28 12:19:00 +0800

    202404281219

--- secret_dungeon_schedule.sql
+++ secret_dungeon_schedule.sql
@@ -1,1 +1,2 @@
 CREATE TABLE 'secret_dungeon_schedule' ('dungeon_area_id' INTEGER NOT NULL, 'teaser_time' TEXT NOT NULL, 'start_time' TEXT NOT NULL, 'count_start_time' TEXT NOT NULL, 'end_time' TEXT NOT NULL, 'close_time' TEXT NOT NULL, PRIMARY KEY('dungeon_area_id'));
+INSERT INTO `secret_dungeon_schedule` VALUES (/*dungeon_area_id*/32001, /*teaser_time*/'2024/05/15 11:00:00', /*start_time*/'2024/05/17 05:00:00', /*count_start_time*/'2024/05/21 04:59:59', /*end_time*/'2024/05/22 04:59:59', /*close_time*/'2024/05/25 14:59:59');

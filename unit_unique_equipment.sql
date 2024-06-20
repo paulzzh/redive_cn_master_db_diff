b263d8ec23fddcbb7d33941cfae41681e6ecb87c
KaguyaMoeBot <104998995+KaguyaMoeBot@users.noreply.github.com>
2024-06-20 17:36:00 +0800

    202406201736

--- unit_unique_equipment.sql
+++ unit_unique_equipment.sql
@@ -151,6 +151,4 @@ INSERT INTO `unit_unique_equipment` VALUES (/*unit_id*/170201, /*equip_slot*/1, 
 INSERT INTO `unit_unique_equipment` VALUES (/*unit_id*/116701, /*equip_slot*/1, /*equip_id*/131671);
 INSERT INTO `unit_unique_equipment` VALUES (/*unit_id*/116801, /*equip_slot*/1, /*equip_id*/131681);
 INSERT INTO `unit_unique_equipment` VALUES (/*unit_id*/116901, /*equip_slot*/1, /*equip_id*/131691);
-INSERT INTO `unit_unique_equipment` VALUES (/*unit_id*/117001, /*equip_slot*/1, /*equip_id*/131701);
-INSERT INTO `unit_unique_equipment` VALUES (/*unit_id*/117101, /*equip_slot*/1, /*equip_id*/131711);
 CREATE INDEX 'unit_unique_equipment_0_unit_id' on 'unit_unique_equipment'('unit_id');

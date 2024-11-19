0f9074383958e43a818d124eac1577fc10b84899
KaguyaMoeBot <104998995+KaguyaMoeBot@users.noreply.github.com>
2024-11-19 10:52:00 +0800

    202411191052

--- unit_unique_equipment.sql
+++ unit_unique_equipment.sql
@@ -164,6 +164,9 @@ INSERT INTO `unit_unique_equipment` VALUES (/*unit_id*/117901, /*equip_slot*/1, 
 INSERT INTO `unit_unique_equipment` VALUES (/*unit_id*/118001, /*equip_slot*/1, /*equip_id*/131801);
 INSERT INTO `unit_unique_equipment` VALUES (/*unit_id*/119001, /*equip_slot*/1, /*equip_id*/131901);
 INSERT INTO `unit_unique_equipment` VALUES (/*unit_id*/119101, /*equip_slot*/1, /*equip_id*/131911);
+INSERT INTO `unit_unique_equipment` VALUES (/*unit_id*/119201, /*equip_slot*/1, /*equip_id*/131921);
+INSERT INTO `unit_unique_equipment` VALUES (/*unit_id*/119301, /*equip_slot*/1, /*equip_id*/131931);
+INSERT INTO `unit_unique_equipment` VALUES (/*unit_id*/119901, /*equip_slot*/1, /*equip_id*/131991);
 INSERT INTO `unit_unique_equipment` VALUES (/*unit_id*/180101, /*equip_slot*/1, /*equip_id*/138011);
 INSERT INTO `unit_unique_equipment` VALUES (/*unit_id*/180601, /*equip_slot*/1, /*equip_id*/138061);
 CREATE INDEX 'unit_unique_equipment_0_unit_id' on 'unit_unique_equipment'('unit_id');

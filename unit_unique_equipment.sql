507adc774f802d05b621cee26ae7590b383603c8
KaguyaMoeBot <104998995+KaguyaMoeBot@users.noreply.github.com>
2025-04-17 15:22:00 +0800

    202504171522

--- unit_unique_equipment.sql
+++ unit_unique_equipment.sql
@@ -181,9 +181,6 @@ INSERT INTO `unit_unique_equipment` VALUES (/*unit_id*/121501, /*equip_slot*/1, 
 INSERT INTO `unit_unique_equipment` VALUES (/*unit_id*/121601, /*equip_slot*/1, /*equip_id*/132161);
 INSERT INTO `unit_unique_equipment` VALUES (/*unit_id*/121901, /*equip_slot*/1, /*equip_id*/132191);
 INSERT INTO `unit_unique_equipment` VALUES (/*unit_id*/122001, /*equip_slot*/1, /*equip_id*/132201);
-INSERT INTO `unit_unique_equipment` VALUES (/*unit_id*/122101, /*equip_slot*/1, /*equip_id*/132211);
-INSERT INTO `unit_unique_equipment` VALUES (/*unit_id*/122201, /*equip_slot*/1, /*equip_id*/132221);
-INSERT INTO `unit_unique_equipment` VALUES (/*unit_id*/122301, /*equip_slot*/1, /*equip_id*/132231);
 INSERT INTO `unit_unique_equipment` VALUES (/*unit_id*/180101, /*equip_slot*/1, /*equip_id*/138011);
 INSERT INTO `unit_unique_equipment` VALUES (/*unit_id*/180301, /*equip_slot*/1, /*equip_id*/138031);
 INSERT INTO `unit_unique_equipment` VALUES (/*unit_id*/180601, /*equip_slot*/1, /*equip_id*/138061);

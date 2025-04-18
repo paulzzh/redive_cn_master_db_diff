7b1e844b24a49a0e08b0a49c15c817fc6b753b2b
KaguyaMoeBot <104998995+KaguyaMoeBot@users.noreply.github.com>
2025-04-18 12:54:00 +0800

    202504181254

--- event_gacha_data.sql
+++ event_gacha_data.sql
@@ -117,7 +117,5 @@ INSERT INTO `event_gacha_data` VALUES (/*gacha_id*/10116, /*event_id*/10116, /*g
 INSERT INTO `event_gacha_data` VALUES (/*gacha_id*/10117, /*event_id*/10117, /*gacha_name*/'交出宝物！　神出鬼没的怪盗', /*item_type*/2, /*item_id*/61162, /*cost*/1, /*repeat_step*/6);
 INSERT INTO `event_gacha_data` VALUES (/*gacha_id*/10118, /*event_id*/10118, /*gacha_name*/'连结・这片・碧蓝　直至少女到达天际', /*item_type*/2, /*item_id*/61172, /*cost*/1, /*repeat_step*/6);
 INSERT INTO `event_gacha_data` VALUES (/*gacha_id*/10119, /*event_id*/10119, /*gacha_name*/'海盗逸话　海盗岛被诅咒的遗宝', /*item_type*/2, /*item_id*/61182, /*cost*/1, /*repeat_step*/6);
-INSERT INTO `event_gacha_data` VALUES (/*gacha_id*/10120, /*event_id*/10120, /*gacha_name*/'吸血鬼传闻　夜与美的盛宴', /*item_type*/2, /*item_id*/61192, /*cost*/1, /*repeat_step*/6);
-INSERT INTO `event_gacha_data` VALUES (/*gacha_id*/10121, /*event_id*/10121, /*gacha_name*/'Enjoy&Refresh！性格迥异的女子露营', /*item_type*/2, /*item_id*/61202, /*cost*/1, /*repeat_step*/6);
 INSERT INTO `event_gacha_data` VALUES (/*gacha_id*/19003, /*event_id*/10003, /*gacha_name*/'吸血鬼猎人with伊莉亚讨伐证交换', /*item_type*/2, /*item_id*/60022, /*cost*/1, /*repeat_step*/5);
 CREATE INDEX 'event_gacha_data_0_event_id' on 'event_gacha_data'('event_id');

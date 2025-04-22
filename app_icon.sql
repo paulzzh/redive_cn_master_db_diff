5800f77145be47895360fd53693efda788ee6722
KaguyaMoeBot <104998995+KaguyaMoeBot@users.noreply.github.com>
2025-04-22 16:19:00 +0800

    202504221619

--- app_icon.sql
+++ app_icon.sql
@@ -77,5 +77,4 @@ INSERT INTO `app_icon` VALUES (/*id*/118231);
 INSERT INTO `app_icon` VALUES (/*id*/118531);
 INSERT INTO `app_icon` VALUES (/*id*/123031);
 INSERT INTO `app_icon` VALUES (/*id*/123331);
-INSERT INTO `app_icon` VALUES (/*id*/125631);
 INSERT INTO `app_icon` VALUES (/*id*/125831);

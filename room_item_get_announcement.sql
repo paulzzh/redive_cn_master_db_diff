e3de8756ed4e8134a1dbcc42deb06232bb50cc20
KaguyaMoeBot <104998995+KaguyaMoeBot@users.noreply.github.com>
2025-06-10 15:19:00 +0800

    202506101519

--- room_item_get_announcement.sql
+++ room_item_get_announcement.sql
@@ -15,3 +15,5 @@ INSERT INTO `room_item_get_announcement` VALUES (/*id*/17, /*room_item_id*/2834,
 INSERT INTO `room_item_get_announcement` VALUES (/*id*/18, /*room_item_id*/2835, /*start_date*/'2025/04/15 11:00:00', /*end_date*/'2099/12/31 23:59:59', /*get_date*/'', /*room_announcement_name*/'');
 INSERT INTO `room_item_get_announcement` VALUES (/*id*/19, /*room_item_id*/2836, /*start_date*/'2025/04/15 11:00:00', /*end_date*/'2099/12/31 23:59:59', /*get_date*/'', /*room_announcement_name*/'');
 INSERT INTO `room_item_get_announcement` VALUES (/*id*/20, /*room_item_id*/2837, /*start_date*/'2025/04/15 11:00:00', /*end_date*/'2099/12/31 23:59:59', /*get_date*/'', /*room_announcement_name*/'');
+INSERT INTO `room_item_get_announcement` VALUES (/*id*/21, /*room_item_id*/2839, /*start_date*/'2025/06/30 11:00:00', /*end_date*/'2099/12/31 23:59:59', /*get_date*/'', /*room_announcement_name*/'');
+INSERT INTO `room_item_get_announcement` VALUES (/*id*/22, /*room_item_id*/2840, /*start_date*/'2025/06/30 11:00:00', /*end_date*/'2099/12/31 23:59:59', /*get_date*/'', /*room_announcement_name*/'');

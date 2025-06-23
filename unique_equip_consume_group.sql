21fa129b05b112e4b6829dc03f8dc47dcebe9eb6
KaguyaMoeBot <104998995+KaguyaMoeBot@users.noreply.github.com>
2025-06-23 16:34:00 +0800

    202506231634

--- unique_equip_consume_group.sql
+++ unique_equip_consume_group.sql
@@ -4,6 +4,7 @@ INSERT INTO `unique_equip_consume_group` VALUES (/*group_id*/2, /*index_in_group
 INSERT INTO `unique_equip_consume_group` VALUES (/*group_id*/3, /*index_in_group*/1, /*item_id*/32048);
 INSERT INTO `unique_equip_consume_group` VALUES (/*group_id*/4, /*index_in_group*/1, /*item_id*/32016);
 INSERT INTO `unique_equip_consume_group` VALUES (/*group_id*/5, /*index_in_group*/1, /*item_id*/32060);
+INSERT INTO `unique_equip_consume_group` VALUES (/*group_id*/5, /*index_in_group*/2, /*item_id*/32078);
 CREATE UNIQUE INDEX 'unique_equip_consume_group_0_group_id_1_index_in_group' on 'unique_equip_consume_group'('group_id','index_in_group');
 CREATE INDEX 'unique_equip_consume_group_0_group_id' on 'unique_equip_consume_group'('group_id');
 CREATE UNIQUE INDEX 'unique_equip_consume_group_0_item_id' on 'unique_equip_consume_group'('item_id');

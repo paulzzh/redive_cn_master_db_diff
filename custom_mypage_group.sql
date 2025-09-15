764b1de7afa6b0df68a28140c3235833c738050a
KaguyaMoeBot <104998995+KaguyaMoeBot@users.noreply.github.com>
2025-09-15 16:16:00 +0800

    202509151616

--- custom_mypage_group.sql
+++ custom_mypage_group.sql
@@ -14,7 +14,7 @@ INSERT INTO `custom_mypage_group` VALUES (/*group_id*/20003, /*group_name*/'公�
 INSERT INTO `custom_mypage_group` VALUES (/*group_id*/20004, /*group_name*/'公主连结！Re:Dive 4周年纪念');
 INSERT INTO `custom_mypage_group` VALUES (/*group_id*/20005, /*group_name*/'公主连结！Re:Dive 4.5周年纪念');
 INSERT INTO `custom_mypage_group` VALUES (/*group_id*/20006, /*group_name*/'公主连结！Re:Dive 5周年纪念');
-INSERT INTO `custom_mypage_group` VALUES (/*group_id*/20007, /*group_name*/'公主连结！Re：Dive 6周年纪念');
+INSERT INTO `custom_mypage_group` VALUES (/*group_id*/20007, /*group_name*/'公主连结！Re：Dive 5.5周年纪念');
 INSERT INTO `custom_mypage_group` VALUES (/*group_id*/29906, /*group_name*/'公主连结！Re:Dive 简中服5周年特别纪念');
 INSERT INTO `custom_mypage_group` VALUES (/*group_id*/30001, /*group_name*/'小游戏·迷你内容');
 INSERT INTO `custom_mypage_group` VALUES (/*group_id*/40001, /*group_name*/'大家的日历');

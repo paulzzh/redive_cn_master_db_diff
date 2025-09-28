bf9191e2568f08a309e4e530f487ae42f3c12228
KaguyaMoeBot <104998995+KaguyaMoeBot@users.noreply.github.com>
2025-09-28 17:57:00 +0800

    202509281757

--- chara_fortune_schedule.sql
+++ chara_fortune_schedule.sql
@@ -6,6 +6,7 @@ INSERT INTO `chara_fortune_schedule` VALUES (/*fortune_id*/10007, /*name*/'4周�
 INSERT INTO `chara_fortune_schedule` VALUES (/*fortune_id*/10008, /*name*/'夏日庆典纪念 兰德索尔杯', /*start_time*/'2024/08/15 5:00:00', /*end_time*/'2024/09/01 4:59:59');
 INSERT INTO `chara_fortune_schedule` VALUES (/*fortune_id*/10009, /*name*/'5周年纪念 兰德索尔杯', /*start_time*/'2025/04/17 5:00:00', /*end_time*/'2025/05/01 4:59:59');
 INSERT INTO `chara_fortune_schedule` VALUES (/*fortune_id*/10010, /*name*/'夏日庆典纪念 兰德索尔杯', /*start_time*/'2025/06/14 5:00:00', /*end_time*/'2025/07/01 4:59:59');
+INSERT INTO `chara_fortune_schedule` VALUES (/*fortune_id*/10011, /*name*/'5.5周年纪念 特别登录奖励', /*start_time*/'2025/10/17 5:00:00', /*end_time*/'2025/11/01 4:59:59');
 INSERT INTO `chara_fortune_schedule` VALUES (/*fortune_id*/18001, /*name*/'第1.5届 兰德索尔杯', /*start_time*/'2021/02/11 5:00', /*end_time*/'2021/02/18 4:59');
 INSERT INTO `chara_fortune_schedule` VALUES (/*fortune_id*/18002, /*name*/'1周年纪念 兰德索尔杯', /*start_time*/'2021/04/18 5:00', /*end_time*/'2021/05/02 4:59');
 INSERT INTO `chara_fortune_schedule` VALUES (/*fortune_id*/18003, /*name*/'端午节 兰德索尔杯', /*start_time*/'2021/06/11 5:00', /*end_time*/'2021/06/21 4:59');
--- tips.sql
+++ tips.sql
@@ -137,6 +137,7 @@ INSERT INTO `tips` VALUES (/*id*/4127201, /*value*/127201, /*tips_index*/1, /*ti
 INSERT INTO `tips` VALUES (/*id*/4127301, /*value*/127301, /*tips_index*/1, /*title*/'一起感受青春');
 INSERT INTO `tips` VALUES (/*id*/4127501, /*value*/127501, /*tips_index*/1, /*title*/'好多礼物啊');
 INSERT INTO `tips` VALUES (/*id*/4127601, /*value*/127601, /*tips_index*/1, /*title*/'虽然有实用性……');
+INSERT INTO `tips` VALUES (/*id*/4129001, /*value*/129001, /*tips_index*/1, /*title*/'不能输');
 INSERT INTO `tips` VALUES (/*id*/4170101, /*value*/170101, /*tips_index*/1, /*title*/'闪亮登场');
 INSERT INTO `tips` VALUES (/*id*/4170201, /*value*/170201, /*tips_index*/1, /*title*/'努力打年糕');
 INSERT INTO `tips` VALUES (/*id*/4180101, /*value*/180101, /*tips_index*/1, /*title*/'猜拳时间！');
